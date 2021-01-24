2f295850b10a9272d8e182c3807f12b459050e5a
Daria0109 <da6ulya888@gmail.com>
2021-01-24 23:46:01 +0300

    App file structure changed

--- js/culculator.js
+++ js/culculator.js
@@ -1,21 +1,3 @@
-// Ttheme modes handling
-const calcButtons = document.querySelectorAll(".calc__button");
-const spanButtons = document.querySelectorAll(".span");
-const calcDisplay = document.querySelector(".calc__display");
-const modeButton = document.querySelector(".mode");
-
-modeButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  modeButton.classList.toggle("dark");
-  calcDisplay.classList.toggle("dark");
-  for (let calcBtn of calcButtons) {
-    calcBtn.classList.toggle("dark");
-  }
-  for (let spanBtn of spanButtons) {
-    spanBtn.classList.toggle("dark-span");
-  }
-});
-
 // Calculator handling
 class Calculator {
   constructor(prevDisplayElement, currentDisplayElement) {
